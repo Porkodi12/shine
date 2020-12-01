2f8c1ea8dd4b94c0276213d60dff7ff2689d468c
Porkodi12 <52107778+Porkodi12@users.noreply.github.com>
2020-12-01 14:18:16 +0530

    Add files via upload

---
+++ admin/nav.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- STYLES CSS -->
+        <!-- BOX ICONS CSS-->
+        <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+        <link href='../css/nav.css' rel='stylesheet'>
+        <title>TUITION MANAGEMENT</title>
+    </head>
+    <form action="" method="POST">
+    <body id="body">
+        <div class="l-navbar" id="navbar" >
+            <nav class="nav">
+                <div id="page-wrapper" style="overflow:hi">
+                    <a href="" class="nav__logo">
+                        <img src="assets/icons/logo.svg" alt="" class="nav__logo-icon">
+                        <span class="nav__logo-text">SHINE</span>
+                    </a>
+    
+                    <div class="nav__toggle" id="nav-toggle">
+                        <i class='bx bx-chevron-right'></i>
+                    </div>
+    
+                    <ul class="nav__list">
+                        <a href="coursemanage.php" class="nav__link">
+                            <i class='bx bx-grid-alt nav__icon'></i>
+                            <span class="nav__text">Home</span>
+
+                        </a>
+                        <a href="admission.php" class="nav__link">
+                            <i class='bx bx-edit nav__icon' ></i>
+                            <span class="nav__text">
+                            Admission  
+                            </span>
+                        </a>
+                        <a href="add_staff.php" class="nav__link">
+                            <i class='bx bx-user-plus nav__icon' ></i>
+                            <span class="nav__text">Add Staff</span>
+                        </a>
+                        <a href="attendance
+                        .php" class="nav__link">
+                            <i class='bx bx-calendar-check nav__icon' ></i>
+                            <span class="nav__text">Attendance</span>
+                        </a>
+                        <a href="add_fees.php" class="nav__link">
+                            <i class='bx bx-money nav__icon' ></i>
+                            <span class="nav__text">Pay Fees</span>
+                        </a>
+                        <a href="add_fees_type.php" class="nav__link">
+                            <i class='bx bx-bell nav__icon' ></i>
+                            <span class="nav__text">Add Fees <br> 
+                            Type</span>
+                        </a>
+                        <a href="announcement.php" class="nav__link">
+                            <i class='bx bx-bell nav__icon' ></i>
+                            <span class="nav__text">Announcement</span>
+                        </a>
+                        <a href="liveclass.php" class="nav__link">
+                            <i class='bx bx-heart nav__icon'></i>
+                            <span class="nav__text">
+                               Live <br>  Classes 
+                            </span>
+                        </a>
+                        <a href="markuptation.php" class="nav__link">
+                            <i class='bx bx-award nav__icon'></i>
+                              <span class="nav__text">Student<br>
+                               Marks</span>
+                        </a>
+                        <!-- <a href="staff_fee.php" class="nav__link">
+                            <i class='bx bx-message-rounded nav__icon' ></i>
+                            <span class="nav__text">
+                                Add <br> Fees
+                            </span>
+                        </a>                  -->
+                        <a href="index.php" class="nav__link">           
+                    <i class='bx bx-log-out-circle nav__icon'></i>
+                    <span class="nav__text">Close</span>
+                </a>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div id="page-wrapper">
+        </div>
+       </div>
+      </div>
+    </body>
+  </form>
+    <!-- MAIN JS -->
+    <script src="../js/nav.js"></script>
+</html>   
